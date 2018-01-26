11b5af47ebed199ebb7029343ccd7cd0c41a1d77
Laurendy Lam <lau_lam@encs.concordia.ca>
2018-01-25 21:03:41 -0500

    initialize maven project

---
+++ src/main/java/app/candycrisis/App.java
@@ -0,0 +1,13 @@
+package app.candycrisis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
