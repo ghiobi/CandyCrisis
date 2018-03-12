fd5073eca2f3420af28c5eb7f3e53ff3f758f072
ziadyrb <ziadyrb@outlook.com>
2018-03-12 04:35:47 -0400

    Heuristic function. Counts how many characters don't match eachother. In the column (a,b,c) If a and c don't match, add 1. If a and b don't match add 1. And if b and c don't match, add 1. It doesn't add in any case if either of the two is the empty character.

--- src/main/java/app/candycrisis/player/SuperSolver.java
+++ src/main/java/app/candycrisis/player/SuperSolver.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public class SuperSolver implements Player {
 
+<<<<<<< HEAD
     enum Action { UP, RIGHT, LEFT, DOWN };
 
     private List<Action> solution;
@@ -114,5 +115,154 @@ public class SuperSolver implements Player {
     public void end() {
 
     }
+=======
+	enum Action { UP, RIGHT, LEFT, DOWN };
+
+	private List<Action> solution;
+
+	private int step;
+
+	@Override
+	public void init(Game init) {
+
+		SearchProblem problem = ProblemBuilder.create()
+				.initialState(init)
+				.defineProblemWithExplicitActions()
+				.useActionFunction(new ActionFunction<Action, Game>() {
+					@Override
+					public Iterable<Action> actionsFor(Game state) {
+						// Here we compute the valid movements for the state
+						return validMovementsFor(state);
+					}
+				})
+				.useTransitionFunction(new ActionStateTransitionFunction<Action, Game>() {
+					@Override
+					public Game apply(Action action, Game state) {
+						// Here we compute the state that results from doing an action A to the current state
+						return applyActionToState(action, state);
+					}
+				})
+				.useCostFunction(new CostFunction<Action, Game, Double>() {
+					@Override
+					public Double evaluate(Transition<Action, Game> transition) {
+						// Every movement has the same cost, 1
+						return 1d;
+					}
+				})
+				.useHeuristicFunction(new HeuristicFunction<Game, Double>() {
+					@Override
+					public Double estimate(Game game) {
+						int count = 0;
+						Piece[] pieces = game.getPieces();
+
+
+						for (int i = 0; i < 5; i++) {
+
+							if (pieces[i].getCharacter() != pieces[i + 10].getCharacter()) {
+								if(piece[i].getCharacter() != EmptyPiece.EMPTY_PIECE_CHARACTER && piece[i+10].getCharacter() != EmptyPiece.EMPTY_PIECE_CHARACTER){
+									count++;
+								}
+							}
+
+							if ((pieces[i].getCharacter() != pieces[i + 10].getCharacter())&&(pieces[i].getCharacter() != pieces[i + 5].getCharacter())) {
+								if(piece[i].getCharacter() != EmptyPiece.EMPTY_PIECE_CHARACTER && piece[i+5].getCharacter() != EmptyPiece.EMPTY_PIECE_CHARACTER){
+									count++;
+								}
+							}
+
+							if ((pieces[i].getCharacter() != pieces[i + 10].getCharacter())&&(pieces[i+5].getCharacter() != pieces[i + 10].getCharacter())) {
+								if(piece[i+5].getCharacter() != EmptyPiece.EMPTY_PIECE_CHARACTER && piece[i+10].getCharacter() != EmptyPiece.EMPTY_PIECE_CHARACTER){
+									count++;
+								}
+							}
+
+						}
+
+						return (double) count;
+					}
+				})
+				.build();
+
+		Algorithm.SearchResult answer = Hipster.createAStar(problem).search(new Predicate<WeightedNode>() {
+			@Override
+			public boolean apply(WeightedNode node) {
+				return ((Game) node.state()).isEndGame();
+			}
+		});
+
+		solution = Lists.reverse(Algorithm.recoverActionPath(answer.getGoalNode()));
+		step = 0;
+	}
+
+	private static Iterable<Action> validMovementsFor(Game state) {
+		List<Action> actions = new LinkedList<>();
+		int[] positions = state.getEmptyPiece().getNeighboringPositions();
+
+		for (int i = 0; i < positions.length; i++) {
+			if (positions[i] != Piece.OUT_OF_BOUNDS_POSITION) {
+				switch (i) {
+				case 0:
+					actions.add(Action.UP);
+					continue;
+				case 1:
+					actions.add(Action.RIGHT);
+					continue;
+				case 2:
+					actions.add(Action.DOWN);
+					continue;
+				case 3:
+					actions.add(Action.LEFT);
+				}
+			}
+		}
+
+		return actions;
+	}
+
+	private static Game applyActionToState(Action action, Game board) {
+		Game successor = board.clone();
+		Piece[] pieces = successor.getPieces();
+		Piece move = null;
+		switch (action){
+		case UP:
+			move = pieces[successor.getEmptyPiece().getNeighboringTopPosition()];
+			break;
+		case DOWN:
+			move = pieces[successor.getEmptyPiece().getNeighboringBottomPosition()];
+			break;
+		case LEFT:
+			move = pieces[successor.getEmptyPiece().getNeighboringLeftPosition()];
+			break;
+		case RIGHT:
+			move = pieces[successor.getEmptyPiece().getNeighboringRightPosition()];
+			break;
+		}
+		try {
+			successor.move(move);
+		} catch (IllegalPuzzleMoveException e) {
+			e.printStackTrace();
+		}
+		return successor;
+	}
+
+	@Override
+	public Piece getMove(Game game) {
+		Piece[] pieces = game.getArrayOfMoves();
+		switch (solution.get(step++)) {
+		case UP:
+			return pieces[0];
+		case RIGHT:
+			return pieces[1];
+		case DOWN:
+			return pieces[2];
+		}
+		return pieces[3];
+	}
+
+	@Override
+	public void end() {
+
+	}
+>>>>>>> Heuristic function.
 
 }
